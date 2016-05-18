c8a86c526601dcf862a769b88cdd029b035d566e
ogaoga <info@ogaoga.org>
2016-05-19 06:04:14 +0900

    Added ui elements.

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import TextArea from './TextArea';
+import ControlsArea from './ControlsArea';
+import VisualizedData from './VisualizedData';
+import AdArea from './AdArea';
+
 export default class Page extends React.Component {
 	render() {
 		return (
-			<div>Page</div>
+      <div className="container">
+        <section className="column json-column">
+          <TextArea />
+          <ControlsArea />
+        </section>
+			  <section className="column data-column">
+          <VisualizedData />
+        </section>
+			  <section className="column ad-column">
+          <AdArea />
+        </section>
+      </div>
 		);
 	}
 }
---
+++ src/VisualizedData.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class VisualizedData extends React.Component {
+	render() {
+		return (
+      <div>
+        <span>VisualizedData</span>
+      </div>
+		);
+	}
+}
