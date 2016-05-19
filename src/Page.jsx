8b6400b7263dd10ee077c63cba30063d0c8eceaf
ogaoga <info@ogaoga.org>
2016-05-19 21:11:50 +0900

    Layouted with mdl.

--- src/Page.jsx
+++ src/Page.jsx
@@ -9,17 +9,27 @@ import AdArea from './AdArea';
 export default class Page extends React.Component {
 	render() {
 		return (
-      <div className="container">
-        <section className="column json-column">
-          <TextArea />
-          <ControlsArea />
-        </section>
-			  <section className="column data-column">
-          <VisualizedData />
-        </section>
-			  <section className="column ad-column">
-          <AdArea />
-        </section>
+      <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+        <header className="mdl-layout__header">
+          <div className="mdl-layout__header-row">
+		        <h1 className="mdl-layout-title">JSON Visual Editor</h1>
+          </div>
+        </header>
+        <main className="container mdl-layout__content">
+          <section className="column json-column">
+            <TextArea />
+            <ControlsArea />
+          </section>
+			    <section className="column data-column">
+            <VisualizedData />
+          </section>
+			    <section className="column ad-column">
+            <AdArea />
+          </section>
+        </main>
+        <footer>
+          <span>Copyright &copy; 2016 ogaoga.org</span>
+        </footer>
       </div>
 		);
 	}
