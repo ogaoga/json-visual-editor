cb8feb9fc85c596909ff22a6a7d47cdc77236e67
ogaoga <info@ogaoga.org>
2016-05-21 00:22:33 +0900

    Stlying UI.

--- src/Page.jsx
+++ src/Page.jsx
@@ -19,6 +19,7 @@ export default class Page extends React.Component {
           <div className="mdl-grid">
             <section className="json-text mdl-cell mdl-cell--4-col">
               <TextArea />
+              <ControlsArea />
             </section>
             <section className="visualized-data mdl-cell mdl-cell--8-col">
               <VisualizedData />
