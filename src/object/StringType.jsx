61a8abcb964e30ca2e362a718f77ec9899de371a
ogaoga <info@ogaoga.org>
2016-06-02 17:38:57 +0900

    ESLint (WIP).

--- src/object/StringType.jsx
+++ src/object/StringType.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 export default class StringType extends React.Component {
 
-	render() {
+  render() {
     let data = this.props.data;
     return (
       <span className="string-type">"{data}"</span>
