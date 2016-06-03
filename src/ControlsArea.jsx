4f4ce9ff79276a559e33fd2395d22b986d3284c8
ogaoga <info@ogaoga.org>
2016-06-03 17:34:39 +0900

    Changed buttons and added sample button.

--- src/ControlsArea.jsx
+++ src/ControlsArea.jsx
@@ -15,16 +15,30 @@ export default class ControlsArea extends React.Component {
           <button id="copy-to-clipboard"
                   data-clipboard-target="#json-text"
                   disabled={this.props.text.length==0}
-                  className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Copy</button>
+                  className="mdl-button mdl-js-button mdl-button--icon"
+                  title="Copy">
+            <i className="material-icons">content_copy</i>
+          </button>
         </div>
         <div className="float-left">
-          <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect"
+          <button className="mdl-button mdl-js-button mdl-button--icon"
                   disabled={this.props.text.length==0}
-                  onClick={this.props.clearText}>Clear</button>
+                  onClick={this.props.clearText}
+                  title="Clear">
+            <i className="material-icons">delete</i>
+          </button>
         </div>
         <div className="float-left">
-          <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect"
-                  onClick={this.props.pasteSample}>Sample</button>
+          <button className="mdl-button mdl-js-button mdl-button--icon"
+                  title="More"
+                  id="more-button">
+            <i className="material-icons">more_vert</i>
+          </button>
+          <ul className="mdl-menu mdl-menu--top-left mdl-js-menu mdl-js-ripple-effect"
+              data-mdl-for="more-button">
+            <li className="mdl-menu__item"
+                onClick={this.props.pasteSample}>Paste Sample</li>
+          </ul>
         </div>
         <div className="float-right control-count">
           <span className="text-count">{this.props.text.length}</span>
