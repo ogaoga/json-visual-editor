4b4aaed7d475016bb5286424a5c98075fa28ea82
ogaoga <tsutomu.ogasawara+github@gmail.com>
2020-05-20 02:08:17 +0900

    Updates

---
+++ precache-manifest.d0af61dcaa6464858d213a2b1d2e4e56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "931811d5d970d8411ef036938a4e210f",
+    "url": "/json-visual-editor/index.html"
+  },
+  {
+    "revision": "c88b130ce24d7674e11f",
+    "url": "/json-visual-editor/static/css/2.a6b6457f.chunk.css"
+  },
+  {
+    "revision": "24995cb22fdbddf3fea7",
+    "url": "/json-visual-editor/static/css/main.d5f81099.chunk.css"
+  },
+  {
+    "revision": "c88b130ce24d7674e11f",
+    "url": "/json-visual-editor/static/js/2.9b539626.chunk.js"
+  },
+  {
+    "revision": "88e934e8173bd1218406a688b7cbb928",
+    "url": "/json-visual-editor/static/js/2.9b539626.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24995cb22fdbddf3fea7",
+    "url": "/json-visual-editor/static/js/main.3fa14d3c.chunk.js"
+  },
+  {
+    "revision": "1205463e190a1cf86c1c",
+    "url": "/json-visual-editor/static/js/runtime-main.c8274472.js"
+  }
+]);
