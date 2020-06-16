2e84f3f8db6480728a7ad935498ea811a21b0651
ogaoga <tsutomu.ogasawara+github@gmail.com>
2020-06-17 01:12:38 +0900

    Update tests

--- src/__tests__/test-object-type.js
+++ src/__tests__/test-object-type.js
@@ -42,9 +42,12 @@ const params = [
     title: 'Display array',
     actual: <ObjectType data={[null, 'abc', 123]} path={[]} />,
     expected: (
-      <table>
+      <table className="">
         <thead data-level="0">
           <tr>
+            <td className="button-cell">
+              <i className="fas fa-plus-circle" data-name=""></i>
+            </td>
             <th className="expand">
               <Expander
                 defaultValue={true}
@@ -56,6 +59,9 @@ const params = [
         </thead>
         <tbody className="expanded">
           <tr>
+            <td className="button-cell">
+              <i className="fas fa-plus-circle" data-name="0"></i>
+            </td>
             <th>
               <span title="0">0</span>
             </th>
@@ -71,6 +77,9 @@ const params = [
             </td>
           </tr>
           <tr>
+            <td className="button-cell">
+              <i className="fas fa-plus-circle" data-name="1"></i>
+            </td>
             <th>
               <span title="1">1</span>
             </th>
@@ -86,8 +95,47 @@ const params = [
             </td>
           </tr>
           <tr>
+            <td className="button-cell">
+              <i className="fas fa-plus-circle" data-name="2"></i>
+            </td>
             <th>
               <span title="2">2</span>
+            </th>
+            <td>
+              <div className="d-flex">
+                <div className="flex-grow-1">
+                  <ObjectType data={123} />
+                </div>
+                <div>
+                  <EditButtons data={123} path={[]} hidden={false} />
+                </div>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    ),
+  },
+  {
+    title: 'Display array w/o insert buttons',
+    actual: <ObjectType data={[123]} path={[]} insert={false} />,
+    expected: (
+      <table className="no-margin">
+        <thead data-level="0">
+          <tr>
+            <th className="expand">
+              <Expander
+                defaultValue={true}
+                onChangeExpansion={function onChangeExpansion() {}}
+              />
+            </th>
+            <th className="objectType">Array [1]</th>
+          </tr>
+        </thead>
+        <tbody className="expanded">
+          <tr>
+            <th>
+              <span title="0">0</span>
             </th>
             <td>
               <div className="d-flex">
@@ -108,9 +156,12 @@ const params = [
     title: 'Display object',
     actual: <ObjectType data={{ abc: 123, xyz: 'abc' }} path={['path']} />,
     expected: (
-      <table>
+      <table className="">
         <thead data-level="1">
           <tr>
+            <td className="button-cell">
+              <i className="fas fa-plus-circle" data-name=""></i>
+            </td>
             <th className="expand">
               <Expander
                 defaultValue={true}
@@ -122,6 +173,9 @@ const params = [
         </thead>
         <tbody className="expanded">
           <tr>
+            <td className="button-cell">
+              <i className="fas fa-plus-circle" data-name="abc"></i>
+            </td>
             <th>
               <span title="path.abc">abc</span>
             </th>
@@ -141,6 +195,9 @@ const params = [
             </td>
           </tr>
           <tr>
+            <td className="button-cell">
+              <i className="fas fa-plus-circle" data-name="xyz"></i>
+            </td>
             <th>
               <span title="path.xyz">xyz</span>
             </th>
@@ -152,6 +209,46 @@ const params = [
                 <div>
                   <EditButtons
                     data={'abc'}
+                    path={['path', 'name']}
+                    hidden={false}
+                  />
+                </div>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    ),
+  },
+  {
+    title: 'Display object w/o insert buttons',
+    actual: <ObjectType data={{ abc: 123 }} path={['path']} insert={false} />,
+    expected: (
+      <table className="no-margin">
+        <thead data-level="1">
+          <tr>
+            <th className="expand">
+              <Expander
+                defaultValue={true}
+                onChangeExpansion={function onChangeExpansion() {}}
+              />
+            </th>
+            <th className="objectType">Object [1]</th>
+          </tr>
+        </thead>
+        <tbody className="expanded">
+          <tr>
+            <th>
+              <span title="path.abc">abc</span>
+            </th>
+            <td>
+              <div className="d-flex">
+                <div className="flex-grow-1">
+                  <ObjectType data={123} path={['path', 'name']} />
+                </div>
+                <div>
+                  <EditButtons
+                    data={123}
                     path={['path', 'name']}
                     hidden={false}
                   />
